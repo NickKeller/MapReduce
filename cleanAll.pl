b14be88e5f98f885c7d5cb518acdca189d8016cb
Nick Keller <nkeller3@gatech.edu>
2016-11-17 11:46:02 -0500

    Added make and clean scripts

---
+++ cleanAll.pl
@@ -0,0 +1,6 @@
+chdir "src/" || die "Couldn't change dir to src/\n$!\n";
+my $return = system("make clean");
+unless($return){
+    chdir "../test/" || die "Couldn't change dir to test/\n$!\n";
+    my $return2 = system("make clean");
+}
